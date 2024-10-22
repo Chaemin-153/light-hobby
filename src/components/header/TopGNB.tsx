57cd1e931d76bccb5034c8f29fe3e57d9b30a01f
Chaemin-153 <0520032@gmail.com>
2024-10-23 04:33:36 +0900

    chore: tailwind.config.js 기초 세팅

--- src/components/header/TopGNB.tsx
+++ src/components/header/TopGNB.tsx
@@ -1,6 +1,6 @@
 const TopGNB = () => {
   return (
-    <div className="flex justify-between bg-yellow-300 text-white text-2xl font-bold p-6">
+    <div className="flex justify-between bg-yellow text-white text-2xl font-bold p-6">
       <div>Light Hobby</div>
       <div className="w-40 flex justify-between text-xl">
         <div>회원가입</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        pc: '1200px',
+        tab: '696px',
+        mob: '343px',
+      },
+      colors: {
+        white: '#FFFFFF',
+        black: '#171717',
+        yellow: '#fed047',
+      },
+      screens: {
+        mob: '500px',
+        sm: { max: '768px' },
+        md: { min: '769px', max: '1023px' },
+        lg: { min: '1024px', max: '1200px' },
+      },
+    },
   },
   plugins: [],
 };
