f13e7f78da734222b8672e7254235ec09b893a18
Chaemin-153 <0520032@gmail.com>
2024-11-05 22:34:46 +0900

    feat: Footer 추가

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/header';
+import Footer from '../components/footer';
 
 const Layout = () => {
   return (
     <div className="flex flex-col w-full">
-      {/* Header 추가 예정 */}
       <Header />
       <Outlet />
-      {/* Footer 추가 예정 */}
-      <h2>Footer</h2>
+      <Footer />
     </div>
   );
 };
