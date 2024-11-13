b47638b6b38a1e84e3131b485dadc68245680845
Chaemin-153 <0520032@gmail.com>
2024-11-13 10:59:02 +0900

    feat: Logo 추가
    TopGNB 컴포넌트에 추가함

--- src/components/header/TopGNB.tsx
+++ src/components/header/TopGNB.tsx
@@ -2,7 +2,14 @@ const TopGNB = () => {
   return (
     <div className="flex justify-center bg-yellow text-white text-2xl sm:text-xl mob:text-base font-bold p-6">
       <div className="flex justify-between w-pc">
-        <div>Light Hobby</div>
+        <div className="flex items-center gap-2">
+          <img
+            className="w-8 h-8"
+            src="/assets/yellowLogoIconImg.png"
+            alt="Light Hobby Logo"
+          />
+          <div>Light Hobby</div>
+        </div>
         <div className="w-40 sm:w-32 mob:w-28 flex justify-between text-xl sm:text-base mob:text-sm">
           <div>회원가입</div>
           <div>로그인</div>
