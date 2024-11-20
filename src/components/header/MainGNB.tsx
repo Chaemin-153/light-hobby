323065fc0514305c00dd1733c433db85ad061a11
Chaemin-153 <0520032@gmail.com>
2024-11-20 14:54:46 +0900

    feat: MainGNB 카테고리 이동 Link 추가

--- src/components/header/MainGNB.tsx
+++ src/components/header/MainGNB.tsx
@@ -1,13 +1,27 @@
+import { Link } from 'react-router-dom';
+
 const MainGNB = () => {
   return (
     <div className="flex justify-center border-b-[1px]">
       <div className="flex justify-around w-pc text-xl sm:text-base mob:text-sm font-bold text-gray-600 p-6 ">
-        <div className="text-yellow border-b-[3px] border-yellow">그림</div>
-        <div>음악</div>
-        <div>운동</div>
-        <div>요리</div>
-        <div>게임</div>
-        <div>영화</div>
+        <Link to={'/category/painting'}>
+          <div className="btn-hover border-b-[4px] border-white">그림</div>
+        </Link>
+        <Link to={'/category/music'}>
+          <div className="btn-hover border-b-[4px] border-white">음악</div>
+        </Link>
+        <Link to={'/category/exercise'}>
+          <div className="btn-hover border-b-[4px] border-white">운동</div>
+        </Link>
+        <Link to={'/category/cooking'}>
+          <div className="btn-hover border-b-[4px] border-white">요리</div>
+        </Link>
+        <Link to={'/category/game'}>
+          <div className="btn-hover border-b-[4px] border-white">게임</div>
+        </Link>
+        <Link to={'/category/movie'}>
+          <div className="btn-hover border-b-[4px] border-white">영화</div>
+        </Link>
       </div>
     </div>
   );
