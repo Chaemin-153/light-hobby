2a75a755f07550bb6c733bc8e27392a108df6aa2
Chaemin-153 <0520032@gmail.com>
2024-12-10 02:06:45 +0900

    feat: 로그인, 회원가입 이동 Link 추가

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const LoginPage = () => {
-  return <div>로그인</div>;
+  return (
+    <div>
+      <div className="flex gap-2 pt-6">
+        <div className="text-[#4B4B4B]">회원이 아니신가요?</div>
+        <Link to="/signup" className="text-yellowHover underline">
+          회원가입하기
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import SignUpForm from '../components/signup/SignUpForm';
 
 const SignupPage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <SignUpForm />
+      <div className="flex gap-2 pt-6">
+        <div className="text-[#4B4B4B]">회원이신가요?</div>
+        <Link to="/login" className="text-yellowHover underline">
+          로그인하기
+        </Link>
+      </div>
     </div>
   );
 };
