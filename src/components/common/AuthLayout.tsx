8640c80441837ab4811e8db3fdcbe458d2a3ac2c
Chaemin-153 <0520032@gmail.com>
2024-12-12 15:49:23 +0900

    style: AuthLayout 높이 수정

--- src/components/common/AuthLayout.tsx
+++ src/components/common/AuthLayout.tsx
@@ -3,11 +3,15 @@ import Footer from '../footer';
 
 const AuthLayout = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full gap-8 mt-8">
-      <Link to={'/'} className="flex justify-center w-1/4">
-        <img src="/assets/whiteLogoImg.png" alt="Light Hobby Logo" />
-      </Link>
-      <Outlet />
+    <div className="flex flex-col justify-center items-center w-full min-h-screen gap-4">
+      <div className="pt-8 w-1/4">
+        <Link to={'/'} className="flex justify-center">
+          <img src="/assets/whiteLogoImg.png" alt="Light Hobby Logo" />
+        </Link>
+      </div>
+      <div className="flex-grow">
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
