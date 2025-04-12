518ca50670ca42c77527f34ba5fd863fd4878e98
Chaemin-153 <0520032@gmail.com>
2025-04-12 09:23:52 +0800

    refactor: HobbyCard Link 설정

--- src/components/common/HobbyCard.tsx
+++ src/components/common/HobbyCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { HobbyData } from '../../types';
 
 interface HobbyDataProps {
@@ -6,18 +7,21 @@ interface HobbyDataProps {
 
 const HobbyCard = ({ hobby }: HobbyDataProps) => {
   const { id, title, views, likes, description, imageUrl } = hobby;
+  const hobbyLink = `/${hobby.category}/${hobby.category + hobby.id}`;
 
   return (
     <div key={id} className="aspect-square w-full">
-      {imageUrl ? (
-        <img
-          src={imageUrl}
-          alt={title}
-          className="w-64 h-64 rounded-xl object-cover"
-        />
-      ) : (
-        <div className="bg-yellow w-full h-64 rounded-xl" />
-      )}
+      <Link to={hobbyLink}>
+        {imageUrl ? (
+          <img
+            src={imageUrl}
+            alt={title}
+            className="w-64 h-64 rounded-xl object-cover"
+          />
+        ) : (
+          <div className="bg-yellow w-full h-64 rounded-xl" />
+        )}
+      </Link>
       <div className="flex flex-col p-4 gap-2">
         <div className="flex font-bold text-lg">{title}</div>
         <div className="flex text-left mob:hidden">{description}</div>
