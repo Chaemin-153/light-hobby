733831f2d08c7df772b48dae8fcb8627a084b48c
Chaemin-153 <0520032@gmail.com>
2025-04-19 22:43:07 +0800

    style: HobbyPost 모바일 스타일 적용

--- src/pages/HobbyPostPage.tsx
+++ src/pages/HobbyPostPage.tsx
@@ -66,101 +66,97 @@ const HobbyPostPage = () => {
           </h2>
         </div>
 
-        <div className="flex flex-col items-center gap-4">
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col gap-6 w-96 h-full mx-auto"
-          >
-            {/* Title Input */}
-            <div className="flex flex-col gap-2 text-left">
-              <label htmlFor="title" className="block text-left">
-                제목
-              </label>
-              <input
-                {...register('title', {
-                  required: '제목을 입력해주세요.',
-                  maxLength: 60,
-                })}
-                type="title"
-                id="title"
-                className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none"
-              />
-              {errors.title && (
-                <small className="text-red-500">{errors.title.message}</small>
-              )}
-            </div>
-            {/* Description Input */}
-            <div className="flex flex-col gap-2 text-left">
-              <label htmlFor="description">설명</label>
-              <textarea
-                {...register('description', {
-                  required: '설명을 입력해주세요.',
-                  maxLength: 1000,
-                })}
-                id="description"
-                rows={6}
-                className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none resize-none"
-              />
-              {errors.description && (
-                <small className="text-red-500">
-                  {errors.description.message}
-                </small>
-              )}
-            </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-6 w-96 sm:w-full mob:w-full h-full mx-auto"
+        >
+          {/* Title Input */}
+          <div className="flex flex-col gap-2 text-left">
+            <label htmlFor="title" className="block text-left">
+              제목
+            </label>
+            <input
+              {...register('title', {
+                required: '제목을 입력해주세요.',
+                maxLength: 60,
+              })}
+              type="title"
+              id="title"
+              className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none"
+            />
+            {errors.title && (
+              <small className="text-red-500">{errors.title.message}</small>
+            )}
+          </div>
+          {/* Description Input */}
+          <div className="flex flex-col gap-2 text-left">
+            <label htmlFor="description">설명</label>
+            <textarea
+              {...register('description', {
+                required: '설명을 입력해주세요.',
+                maxLength: 1000,
+              })}
+              id="description"
+              rows={6}
+              className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none resize-none"
+            />
+            {errors.description && (
+              <small className="text-red-500">
+                {errors.description.message}
+              </small>
+            )}
+          </div>
 
-            {/* Category Dropdown */}
-            <div className="flex flex-col gap-2 text-left">
-              <label htmlFor="category">카테고리</label>
-              <select
-                {...register('category', {
-                  required: '카테고리를 선택해주세요.',
-                })}
-                id="category"
-                className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none"
-              >
-                <option value="">선택하세요</option>
-                <option value="painting">그림</option>
-                <option value="music">음악</option>
-                <option value="exercise">운동</option>
-                <option value="cooking">요리</option>
-                <option value="game">게임</option>
-                <option value="movie">영화</option>
-              </select>
-              {errors.category && (
-                <small className="text-red-500">
-                  {errors.category.message}
-                </small>
-              )}
-            </div>
-
-            {/* Image Upload Input */}
-            <div className="flex flex-col gap-2 text-left">
-              <label htmlFor="image">이미지 업로드</label>
-              <input
-                type="file"
-                id="image"
-                accept="image/*"
-                {...register('image', {
-                  required: '이미지를 선택해주세요',
-                })}
-                placeholder="이미지를 선택해주세요"
-                className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none"
-              />
-              {errors.image && (
-                <small className="text-red-500">{errors.image.message}</small>
-              )}
-            </div>
-
-            {/* Submit Button */}
-            <button
-              type="submit"
-              className="w-full bg-yellow text-white py-2 rounded focus:border-yellow focus:outline-none hover:bg-yellowHover"
-              disabled={isSubmitting}
+          {/* Category Dropdown */}
+          <div className="flex flex-col gap-2 text-left">
+            <label htmlFor="category">카테고리</label>
+            <select
+              {...register('category', {
+                required: '카테고리를 선택해주세요.',
+              })}
+              id="category"
+              className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none"
             >
-              작성 완료
-            </button>
-          </form>
-        </div>
+              <option value="">선택하세요</option>
+              <option value="painting">그림</option>
+              <option value="music">음악</option>
+              <option value="exercise">운동</option>
+              <option value="cooking">요리</option>
+              <option value="game">게임</option>
+              <option value="movie">영화</option>
+            </select>
+            {errors.category && (
+              <small className="text-red-500">{errors.category.message}</small>
+            )}
+          </div>
+
+          {/* Image Upload Input */}
+          <div className="flex flex-col gap-2 text-left">
+            <label htmlFor="image">이미지 업로드</label>
+            <input
+              type="file"
+              id="image"
+              accept="image/*"
+              {...register('image', {
+                required: '이미지를 선택해주세요',
+              })}
+              placeholder="이미지를 선택해주세요"
+              className="w-full px-3 py-2 border rounded focus:border-yellow focus:outline-none"
+            />
+            {errors.image && (
+              <small className="text-red-500">{errors.image.message}</small>
+            )}
+          </div>
+
+          {/* Submit Button */}
+          <button
+            type="submit"
+            className="w-full bg-yellow text-white py-2 rounded focus:border-yellow focus:outline-none hover:bg-yellowHover"
+            disabled={isSubmitting}
+          >
+            작성 완료
+          </button>
+        </form>
       </div>
     </div>
   );
