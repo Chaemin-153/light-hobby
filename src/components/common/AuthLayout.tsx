a6bcc9ad8478ea4979a33218fdb34d55fefadddd
Chaemin-153 <0520032@gmail.com>
2025-05-25 18:28:19 +0800

    style: AuthLayout 스타일 수정(Footer 삭제, 가운데 정렬)

--- src/components/common/AuthLayout.tsx
+++ src/components/common/AuthLayout.tsx
@@ -1,18 +1,16 @@
 import { Link, Outlet } from 'react-router-dom';
-import Footer from '../footer';
 
 const AuthLayout = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-full min-h-screen gap-4">
-      <div className="pt-8 w-1/4">
+    <div className="flex flex-col justify-center items-center w-full min-h-screen gap-4 pb-24">
+      <div className="w-1/4">
         <Link to={'/'} className="flex justify-center">
           <img src="/assets/whiteLogoImg.png" alt="Light Hobby Logo" />
         </Link>
       </div>
-      <div className="flex-grow">
+      <div className="flex flex-col">
         <Outlet />
       </div>
-      <Footer />
     </div>
   );
 };
